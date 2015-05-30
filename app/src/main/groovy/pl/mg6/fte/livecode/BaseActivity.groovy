1a19f3c86ba4c5966a836132e7e1d423b806ae37
Maciej Górski <maciek.gorski@gmail.com>
2015-05-30 21:12:58 +0200

    Add TextInputLayout just for fun and test.

--- app/src/main/groovy/pl/mg6/fte/livecode/BaseActivity.groovy
+++ app/src/main/groovy/pl/mg6/fte/livecode/BaseActivity.groovy
@@ -2,11 +2,12 @@ package pl.mg6.fte.livecode
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import groovy.transform.CompileStatic
 import pl.mg6.fte.livecode.dagger.Injector
 
 @CompileStatic
-abstract class BaseActivity extends Activity {
+abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
