13fffd55c1b5a74240ae7bae3a769790aed424ba
Maciej Górski <maciek.gorski@gmail.com>
2015-05-31 02:16:57 +0200

    Add required User-Agent header.

--- app/src/main/groovy/pl/mg6/fte/livecode/dagger/AppModule.groovy
+++ app/src/main/groovy/pl/mg6/fte/livecode/dagger/AppModule.groovy
@@ -5,6 +5,7 @@ import dagger.Provides
 import groovy.transform.CompileStatic
 import pl.mg6.fte.livecode.MainActivity
 import pl.mg6.fte.livecode.api.GithubApi
+import retrofit.RequestInterceptor.RequestFacade
 import retrofit.RestAdapter
 
 import javax.inject.Singleton
@@ -18,8 +19,14 @@ public class AppModule {
     RestAdapter provideRestAdapter() {
         return new RestAdapter.Builder()
                 .setEndpoint('https://api.github.com')
+                .setRequestInterceptor(this.&onIntercept)
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .build()
+    }
+
+    private void onIntercept(RequestFacade it) {
+        it.addHeader('Accept', 'application/vnd.github.v3+json')
+        it.addHeader('User-Agent', 'https://github.com/mg6maciej/android-fte-livecode')
     }
 
     @Provides
